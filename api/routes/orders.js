7cca0aa20b4b73aedfefc5e5a804abf259e2e4e6
Francine Braga <francine.fbrg@hotmail.com>
2019-08-23 15:01:59 -0300

    Create REST services Create local database Create postman collection to REST services

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,38 +1,71 @@
 const express = require('express');
 const checkAuth = require('../utils/checkauth')
+const notFound = require("../utils/notfound");
 const router = express.Router();
 
+let db = {};
+let sequence = 0;
+
 router.post('/', checkAuth, (request, response) => {
- response.status(200).json({
-   message: 'Handling POST requests to /orders'
- });
+  const newOrder = {
+    id: ++sequence,
+    orderId: request.body.orderId,
+    customerId: request.body.customerId,
+    receiverName: request.body.receiverName,
+    receiverCpf: request.body.receiverCpf,
+    isBuyer: request.body.isBuyer || false,
+    dateTimeDelivery: request.body.dateTimeDelivery,
+    local: request.body.local
+  }
+
+  db[newOrder.id] = newOrder;
+
+  response.status(200).json(newOrder);
 });
 
 router.get('/', (request, response) => {
- response.status(200).json({
-   message: 'Handling GET requests to /orders'
- });
+  const toArray = key => db[key];
+  const orders = Object.keys(db).map(toArray);
+
+  orders.length
+  ? response.json(orders)
+  : response.status(204).send();
 });
 
 router.get('/:orderId', (request, response) => {
- const id = request.params.orderId;
- response.status(200).json({
-   message: `Order with ID = ${id} was fetched`
- });
+  const order = db[request.params.orderId];
+  order
+  ? response.json(order)
+  : notFound(request, response)
 });
 
 router.patch('/:orderId', checkAuth, (request, response) => {
- const id = request.params.orderId;
- response.status(200).json({
-   message: `Order with ID = ${id} was updated`
- });
+  const order = db[request.params.orderId];
+  const hasValue = request.body.isBuyer != null;
+  if (order) {
+    order.orderId = request.body.orderId || order.orderId;
+    order.customerId = request.body.customerId || order.customerId;
+    order.receiverName = request.body.receiverName || order.receiverName;
+    order.receiverCpf = request.body.receiverCpf || order.receiverCpf;
+    order.isBuyer = hasValue ? request.body.isBuyer : order.isBuyer;
+    order.dateTimeDelivery = request.body.dateTimeDelivery || order.dateTimeDelivery;
+    order.local = request.body.local || order.local;
+    response.json(order);
+  } else {
+    notFound(request, response);
+  }
 });
 
 router.delete('/:orderId', checkAuth, (request, response) => {
- const id = request.params.orderId;
- response.status(200).json({
-   message: `Order with ID = ${id} was deleted`
- });
+  const order = db[request.params.orderId];
+  if (order) {
+    delete db[order.id];
+    response.status(200). json({
+      message:'Deleted'
+    });
+  } else {
+    notFound(request, response);
+  }
 });
 
 module.exports = router;
